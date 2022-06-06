d60e918ddd904151d6d6820da23d62255de89638
tduck973564 <tduck973564@gmail.com>
2022-06-06 17:56:02 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -128,7 +128,7 @@ systemctl --user enable onedrive
 systemctl --user start onedrive
 
 echo Download icon theme and fonts
-sudo dnf install -y papirus-icon-theme fira-code-fonts google-roboto-fonts ibm-plex-fonts-all rsms-inter-fonts materia-gtk-theme
+sudo dnf install -y papirus-icon-theme fira-code-fonts google-roboto-fonts ibm-plex-fonts-all rsms-inter-fonts materia-gtk-theme firewall-config
 
 <<comment 
 echo Fix font problems in SDDM
