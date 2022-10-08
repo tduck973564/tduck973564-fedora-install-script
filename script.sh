635d7bb3ca67db0ba7d53c82a87e63bf08c98fd3
tduck973564 <tduck973564@gmail.com>
2022-10-09 00:26:24 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,46 +1,45 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 # Fedora Install Script by tduck973564
-# Makes life a little bit easier
 
-echo CD into home directory
+echo "CD into home directory"
 cd ~
 
-echo Adding Flathub to Flatpak
+echo "Adding Flathub to Flatpak"
 flatpak remote-delete flathub
 flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo Speed up DNF
+echo "Speed up DNF"
 sudo dnf install dnf-plugins-core -y
 sudo sh -c "echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf"
 sudo sh -c "echo 'fastestmirror=True' >> /etc/dnf/dnf.conf"
 
-echo Installation of RPMFusion
+echo "Installation of RPMFusion"
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate -y core
 sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False"
 
-echo Patch GNOME
+echo "Patch GNOME"
 echo "[Settings]\ngtk-hint-font-metrics=1" >> ~/.config/gtk-4.0/settings.ini
 sudo dnf copr enable calcastor/gnome-patched -y
 
-echo Update system before continuing
+echo "Update system before continuing"
 sudo dnf --refresh upgrade -y
 
-echo Xanmod kernel
+echo "Xanmod kernel"
 sudo dnf copr enable rmnscnce/kernel-xanmod -y
 sudo dnf install kernel-xanmod-edge -y
 
-echo Install wine
+echo "Install wine"
 sudo dnf install wine winetricks bottles
 
-echo Installation of Oh My Zsh!
+echo "Installation of Oh My Zsh!"
 sudo dnf install util-linux-user zsh git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 chsh -s /usr/bin/zsh
 sed -e s/robbyrussell/lukerandall/ ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
 
-echo Installation of GitHub CLI and setup of Git
+echo "Installation of GitHub CLI and setup of Git"
 sudo dnf config-manager -y --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf install -y gh
 sh -c "gh auth login"
@@ -53,35 +52,35 @@ read GITEMAIL
 git config --global user.name $GITUSERNAME
 git config --global user.email  $GITEMAIL
 
-echo Installation of VSCode
+echo "Installation of VSCode"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install -y code
 
-echo Installation of Rust
+echo "Installation of Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile default -y
 
-echo Installation of Python
+echo "Installation of Python"
 sudo dnf install python pip
 pip install --upgrade pip
 pip install pylint
 curl -sSL https://install.python-poetry.org | python3 -
 
-echo Installation of TypeScript and JavaScript
+echo "Installation of TypeScript and JavaScript"
 sudo dnf install nodejs
 sudo npm install -g typescript npm
 
-echo Installation of Java
+echo "Installation of Java"
 sudo dnf install -y java-17-openjdk java-17-openjdk-devel
 
-echo Installation of build-essential equivalent, clang, Meson and Ninja
+echo "Installation of build-essential equivalent, clang, Meson and Ninja"
 sudo dnf install -y make automake gcc gcc-c++ kernel-devel clang clang-tools-extra meson ninja-build
 
-echo Installation of JetBrains
+echo "Installation of JetBrains"
 curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
 jetbrains-toolbox
 
-echo Installation of VSCode extensions
+echo "Installation of VSCode extensions"
 code --install-extension ms-python.python
 code --install-extension GitHub.copilot
 code --install-extension GitHub.copilot-labs
@@ -104,25 +103,63 @@ code --install-extension mesonbuild.mesonbuild
 code --uninstall-extension ms-vscode.cpptools
 code --install-extension llvm-vs-code-extensions.vscode-clangd
 
-echo Set VSCode settings
+echo "Installation of apps"
 
-echo Installation of miscellaneous useful apps
-sudo dnf remove -y gnome-terminal
-sudo dnf install -y discord ffmpeg pavucontrol pulseeffects gnome-tweaks firewall-config seahorse gnome-console
-flatpak install -y com.github.tchx84.Flatseal org.gnome.Extensions io.github.realmazharhussain.GdmSettings
+sudo dnf remove -y \
+gnome-terminal \
+rhythmbox \
+fedora-bookmarks
 
-echo Log into accounts on web browser
+sudo dnf install -y \
+discord \
+ffmpeg \
+easyeffects \
+gnome-tweaks \
+firewall-config \
+seahorse \
+gnome-console \
+steam \
+dconf-editor \
+wine \
+bottles \
+builder \
+gcolor3 \
+dialect \
+inkscape \
+kdenlive \
+musescore
+
+flatpak install -y \
+com.github.tchx84.Flatseal \
+org.gnome.Extensions io.github.realmazharhussain.GdmSettings \
+io.bassi.Amberol \
+org.gnome.World.Citations \
+com.github.huluti.Curtail \
+com.belmoussaoui.Decoder \
+dev.Cogitri.Health \
+org.gimp.GIMP \
+com.adrienplazas.Metronome \
+io.github.seadve.Mousai \
+net.pcsx2.PCSX2 \
+com.github.alexhuntley.Plots \
+org.signal.Signal \
+org.gnome.SoundRecorder \
+org.gnome.Solanum \
+com.github.liferooter.textpieces \
+com.github.hugolabe.Wike
+
+echo "Log into accounts on web browser"
 firefox https://accounts.google.com/
 firefox https://login.microsoftonline.com/
 firefox https://discord.com/app
 firefox https://github.com/login
 
-echo Make some folders
+echo "Make some folders"
 mkdir ~/Repositories
 mkdir ~/Coding
 mkdir ~/Games
 
-echo Set up SSH and enable firewall to block all except on port 22
+echo "Set up SSH and enable firewall to block all except on port 22"
 sudo systemctl enable --now sshd
 sudo systemctl enable --now firewalld
 for i in $( ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' ); do
@@ -131,43 +168,26 @@ for i in $( ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' ); do
 done
 sudo firewall-cmd --permanent --add-service=ssh
 
-echo Install nvidia drivers if nvidia gpu is installed
+echo "Install nvidia drivers if nvidia gpu is installed"
 if [[ $(lspci) = *NVIDIA* ]]; then
   sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
   echo "Wait 5 minutes before restarting to make sure the module is built"
 fi
 
-echo Install onedrive
+echo "Install onedrive"
 sudo dnf install -y onedrive
 onedrive
 systemctl --user enable onedrive
 systemctl --user start onedrive
 
-echo Download icon theme and fonts
-sudo dnf install -y papirus-icon-theme fira-code-fonts google-roboto-fonts ibm-plex-fonts-all rsms-inter-fonts materia-gtk-theme
+echo "Download icon theme and fonts"
+sudo dnf install -y papirus-icon-theme fira-code-fonts google-roboto-fonts ibm-plex-fonts-all rsms-inter-fonts
 
-<<comment 
-echo Fix font problems in SDDM
-sudo sh -c 'echo "<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-<alias>
-    <family>sans-serif</family>
-    <prefer>
-    <family>Roboto</family>
-    <family>Noto Sans Symbols</family>
-    <family>Noto Sans Symbols2</family>
-    <family>Noto Emoji</family>
-    </prefer>
-</alias>
-</fontconfig>" > /etc/fonts/local.conf'
-comment
-
-echo Dotfiles
+echo "Dotfiles"
 git clone https://github.com/tduck973564/dotfiles ~/.dotfiles
 echo ". ~/.dotfiles/.aliases" >> ~/.zshrc
 
-echo "Fix inconsistent GNOME 42 theming"
+echo "Fix inconsistent GNOME theming"
 sudo dnf copr enable nickavem/adw-gtk3 -y
 sudo dnf install adw-gtk3
 flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark 
@@ -193,12 +213,29 @@ done
 gnome-extensions disable background-logo@fedorahosted.org
 
 echo "Set theme settings"
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.interface clock-show-weekday true
 
-echo Install firefox theme
+gsettings set org.gnome.desktop.interface document-font-name 'Inter 11'
+gsettings set org.gnome.desktop.interface font-name 'Inter 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'IBM Plex Mono 11'
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
+echo "Install firefox theme"
 cd ~/Repositories
 git clone https://github.com/rafaelmardojai/firefox-gnome-theme
 cd firefox-gnome-theme
 ./scripts/auto-install.sh
 cd ~
+
+echo "Firefox addons"
+wget \ 
+ hhttps://addons.mozilla.org/firefox/downloads/file/4003969/ublock_origin-1.44.4.xpi \ 
+ https://addons.mozilla.org/firefox/downloads/file/3974897/gnome_shell_integration-11.1.xpi \
+ https://addons.mozilla.org/firefox/downloads/file/4010387/bypass_paywalls_clean-2.8.7.0.xpi && 
+firefox *.xpi && rm *.xpi
 
 echo -e '\nDone!'
