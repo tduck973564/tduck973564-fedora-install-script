de207fcb861f40d1442dc58184e5433dfd2a7619
tduck973564 <tduck973564@gmail.com>
2022-12-06 10:07:59 +1100

    Update battery.sh

--- battery.sh
+++ battery.sh
@@ -1,4 +1,6 @@
 # Optimizes battery
+sudo dnf remove power-profiles-daemon
+
 sudo dnf install tlp tlp-rdw powertop
 
 sudo powertop --auto-tune
@@ -8,6 +10,12 @@ sudo systemctl mask systemd-rfkill.service
 sudo systemctl mask systemd-rfkill.socket
 sudo systemctl enable --now NetworkManager-dispatcher
 
+sudo sh -c "echo 'PCIE_ASPM_ON_BAT=powersupersave
+CPU_SCALER_GOVERNOR_ON_BAT=powersave
+PLATFORM_PROFILE_ON_BAT=low-power' >> /etc/tlp.conf"
+
 sudo systemctl enable --now tlp
 sudo tlp-rdw enable
+
+
 
