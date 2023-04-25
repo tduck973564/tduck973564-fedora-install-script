14c609c04de5ad0bea036a030bc7ae14b0f5d16e
tduck973564 <tduck973564@gmail.com>
2023-04-25 20:45:33 +1000

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -71,6 +71,7 @@ gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 
 gsettings set org.gnome.shell had-bluetooth-devices-setup true
+gsettings set org.gnome.software packaging-format-preference "['flatpak:fedora-testing', 'flatpak:fedora', 'flatpak:flathub', 'rpm']"
 
 gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Console.desktop']"
 
