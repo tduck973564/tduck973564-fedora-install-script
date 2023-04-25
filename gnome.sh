df55eaadd9cc0d36af33fb42a5e127d9abb9563a
tduck973564 <tduck973564@gmail.com>
2023-04-25 15:06:10 +1000

    update for fedora 38

--- gnome.sh
+++ gnome.sh
@@ -12,7 +12,7 @@ gnome-tweaks \
 seahorse \
 gnome-console \
 
-flatpak install -y \
+flatpak install -y flathub \
 org.gnome.Extensions io.github.realmazharhussain.GdmSettings \
 io.bassi.Amberol \
 org.gnome.World.Citations \
@@ -38,16 +38,14 @@ org.gnome.Builder
 echo "Fix inconsistent GNOME theming"
 sudo dnf copr enable nickavem/adw-gtk3 -y
 sudo dnf install adw-gtk3
-flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark 
+flatpak install -y flathub org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark 
 
 echo "Install shell extensions"
 gsettings set org.gnome.shell disable-extension-version-validation true
 
-array=( https://extensions.gnome.org/extension/615/appindicator-support/
-https://extensions.gnome.org/extension/3193/blur-my-shell/
+array=( https://extensions.gnome.org/extension/3193/blur-my-shell/
 https://extensions.gnome.org/extension/4135/espresso/
-https://extensions.gnome.org/extension/1723/wintile-windows-10-window-tiling-for-gnome/ 
-https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/ )
+https://extensions.gnome.org/extension/1723/wintile-windows-10-window-tiling-for-gnome/ )
 
 for i in "${array[@]}"
 do
@@ -77,8 +75,7 @@ gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,m
 
 gsettings set org.gnome.shell had-bluetooth-devices-setup true
 
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Nautilus.desktop']"
-gsettings set org.gnome.shell app-picker-layout "[{'discord.desktop': <{'position': <0>}>, 'org.signal.Signal.desktop': <{'position': <1>}>, 'org.gnome.Geary.desktop': <{'position': <2>}>, 'org.gnome.Console.desktop': <{'position': <3>}>, 'io.bassi.Amberol.desktop': <{'position': <4>}>, 'org.gnome.TextEditor.desktop': <{'position': <5>}>, 'org.gnome.Contacts.desktop': <{'position': <6>}>, 'org.gnome.Weather.desktop': <{'position': <7>}>, 'org.gnome.clocks.desktop': <{'position': <8>}>, 'org.gnome.Maps.desktop': <{'position': <9>}>, 'org.gnome.Photos.desktop': <{'position': <10>}>, 'org.gnome.Totem.desktop': <{'position': <11>}>, 'org.gnome.Calculator.desktop': <{'position': <12>}>, 'org.gnome.Cheese.desktop': <{'position': <13>}>, 'app.drey.Dialect.desktop': <{'position': <14>}>, 'org.gnome.SoundRecorder.desktop': <{'position': <15>}>, '151ac8a7-14a6-431f-8e41-6c3a621957c3': <{'position': <16>}>, '4a525d00-5640-4141-af90-14b785c02b2c': <{'position': <17>}>, '746bd76a-c391-41e9-b41e-ea91cd833481': <{'position': <18>}>, '054e367e-7f3f-4682-a7cd-3f8140c11b6c': <{'position': <19>}>, '5aee2c61-06cd-4754-943e-4f876f3a7277': <{'position': <20>}>, 'Utilities': <{'position': <21>}>, 'org.gnome.Software.desktop': <{'position': <22>}>, 'org.gnome.Settings.desktop': <{'position': <23>}>}]"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Console.desktop']"
 
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/org-freedesktop-problems-applet/ enable false
 
