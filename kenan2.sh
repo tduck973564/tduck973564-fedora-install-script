e5ca22ed6554e7a4973471051d86229c872216aa
tduck973564 <tduck973564@gmail.com>
2023-06-05 22:51:35 +1000

    Update kenan2.sh

--- kenan2.sh
+++ kenan2.sh
@@ -18,6 +18,7 @@ echo \"Install RPMFusion\"
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf groupupdate -y core
 dnf groupupdate -y multimedia --setop=\"install_weak_deps=False\"
+dnf install -y ffmpeg --allowerasing
 
 echo \"Update system before continuing\"
 dnf --refresh upgrade -y
