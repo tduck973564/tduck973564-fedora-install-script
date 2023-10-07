a2c0078b772726442e3076179b29fdd9ed13405c
tduck973564 <tduck973564@gmail.com>
2023-10-08 02:26:50 +1100

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,8 +1,4 @@
-echo "Install bismuth"
-
-sudo dnf5 install bismuth
-
-echo "Remove useless apps"
+echo "Remove Akonadi and useless apps"
 
 sudo dnf5 remove pim* akonadi* akregator korganizer kmail ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common kmahjongg kpat kmines
 
