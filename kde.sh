4fc221617572dd468ff5ee07b6e126c36c872caf
tduck973564 <tduck973564@gmail.com>
2024-01-25 01:01:07 +1100

    remove k3b

--- kde.sh
+++ kde.sh
@@ -1,4 +1,4 @@
 echo "Remove Akonadi and useless apps"
 
-sudo dnf5 remove pim* akonadi* akregator krusader korganizer kmail ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common kmahjongg kpat kmines
+sudo dnf5 remove pim* akonadi* akregator krusader korganizer kmail ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common kmahjongg kpat kmines k3b
 
