6ce0aab2a8d8e9f2d2025d9b420fd81149dda9c9
tduck973564 <tduck973564@gmail.com>
2024-03-01 22:11:07 +1100

    Update arch-kde-apps.sh

--- arch-kde-apps.sh
+++ arch-kde-apps.sh
@@ -1,1 +1,7 @@
-sudo pacman -S plasma-meta kde-system-meta kdeconnect kdegraphics-thumbnailers baloo-widgets dolphin-plugins ffmpegthumbs kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt5-vlc xwaylandvideobridge maliit-keyboard xsettingsd xdg-desktop-portal-gtk kpipewire dolphin ark dragon elisa filelight gwenview isoimagewriter kcalc sddm-kcm xdg-desktop-portal-kde kde-gtk-config bluedevil breeze-plymouth flatpak-kcm drkonqi plymouth-kcm kolourpaint spectacle okular kamera kcolorchooser colord-kde kwrite kclock konsole firefox kwalletmanager kweather print-manager skanpage
+sudo pacman -S plasma-meta kde-system-meta kdeconnect kdegraphics-thumbnailers baloo-widgets dolphin-plugins ffmpegthumbs kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt5-vlc xwaylandvideobridge maliit-keyboard xsettingsd xdg-desktop-portal-gtk kpipewire dolphin ark dragon elisa filelight gwenview kcalc sddm-kcm xdg-desktop-portal-kde kde-gtk-config bluedevil breeze-plymouth flatpak-kcm drkonqi plymouth-kcm kolourpaint spectacle okular kamera kcolorchooser colord-kde kwrite kclock konsole firefox kwalletmanager kweather print-manager skanpage kalgebra
+
+FLATPAK_FLATHUB=( org.kde.isoimagewriter
+org.kde.francis )
+for app in ${FLATPAK_FLATHUB[@]}; do
+	flatpak install -y flathub "$app"
+done
