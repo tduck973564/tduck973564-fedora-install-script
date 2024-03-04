d6385638e8b8cdd54dcaac2379601b650aa9def3
tduck973564 <tduck973564@gmail.com>
2024-03-04 20:16:08 +1100

    Update kinoite-install.sh

--- kinoite-install.sh
+++ kinoite-install.sh
@@ -8,11 +8,11 @@ sudo rpm-ostree install https://packagecloud.io/filips/FirefoxPWA/packages/rpm_a
 
 sudo rpm-ostree install plasma-firewall openssl
 
-sudo rpm-ostree install util-linux-user zsh git gh
+sudo rpm-ostree install zsh gh
 
 sudo rpm-ostree kargs --append='nouveau.config=NvGspRm=1'
 
-sudo rpm-ostree install ibm-plex-fonts-all rsms-inter-fonts onedrive
+sudo rpm-ostree install ibm-plex-fonts-all rsms-inter-fonts onedrive epson-inkjet-printer-escpr2
 sudo rpm-ostree install https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 
 FLATPAK_FLATHUB=( org.kde.kalgebra
@@ -21,7 +21,9 @@ org.kde.kclock
 org.kde.kweather
 org.kde.kcolorchooser
 org.kde.francis 
-org.libreoffice.LibreOffice )
+org.libreoffice.LibreOffice
+com.github.wwmm.easyeffects )
+
 for app in ${FLATPAK_FLATHUB[@]}; do
 	flatpak install -y flathub "$app"
 done
