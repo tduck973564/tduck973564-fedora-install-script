b6027f58fab6e88bb076646725e4262c6f157d0a
tduck973564 <tduck973564@gmail.com>
2024-03-04 20:07:54 +1100

    Create kinoite-install.sh

---
+++ kinoite-install.sh
@@ -0,0 +1,31 @@
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sudo rpm-ostree install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo rpm-ostree install https://packagecloud.io/filips/FirefoxPWA/packages/rpm_any/rpm_any/firefoxpwa-2.10.1-1.x86_64.rpm
+
+sudo rpm-ostree install ffmpeg
+
+sudo rpm-ostree install plasma-firewall openssl
+
+sudo rpm-ostree install util-linux-user zsh git gh
+
+sudo rpm-ostree kargs --append='nouveau.config=NvGspRm=1'
+
+sudo rpm-ostree install ibm-plex-fonts-all rsms-inter-fonts onedrive
+sudo rpm-ostree install https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
+
+FLATPAK_FLATHUB=( org.kde.kalgebra
+org.kde.isoimagewriter
+org.kde.kclock 
+org.kde.kweather
+org.kde.kcolorchooser
+org.kde.francis 
+org.libreoffice.LibreOffice )
+for app in ${FLATPAK_FLATHUB[@]}; do
+	flatpak install -y flathub "$app"
+done
+
+kwriteconfig5 --file kdesurc --group super-user-command --key super-user-command sudo
