a53d7c911b4d9e945b82492d7a86c479400f1bd7
tduck973564 <tduck973564@gmail.com>
2024-03-04 20:08:04 +1100

    Create kinoite-postinstall.sh

---
+++ kinoite-postinstall.sh
@@ -0,0 +1,14 @@
+chsh -s /usr/bin/zsh
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+echo "setopt +o nomatch" >> ~/.zshrc
+echo "zmodule bira" >> ~/.zimrc
+
+sh -c "gh auth login"
+
+echo "Type in your git username: "
+read GITUSERNAME
+echo "Type in your git email: "
+read GITEMAIL
+
+git config --global user.name $GITUSERNAME
+git config --global user.email $GITEMAIL
