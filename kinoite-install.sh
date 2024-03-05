958be3fd4907a783cafa4d86cc0e09f9fd0ef670
tduck973564 <tduck973564@gmail.com>
2024-03-05 15:43:38 +1100

    Update kinoite-install.sh

--- kinoite-install.sh
+++ kinoite-install.sh
@@ -4,7 +4,9 @@ sudo rpm-ostree install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo rpm-ostree install 'https://packagecloud.io/filips/FirefoxPWA/packages/rpm_any/rpm_any/firefoxpwa-2.10.1-1.x86_64.rpm?distro_version_id=227'
+curl "https://packagecloud.io/filips/FirefoxPWA/packages/rpm_any/rpm_any/firefoxpwa-2.10.1-1.x86_64.rpm/download.rpm?distro_version_id=227"
+sudo rpm-ostree install ./download.rpm
+rm -f download.rpm
 
 sudo rpm-ostree install plasma-firewall openssl libreoffice
 
