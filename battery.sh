497263969a9cb6f9131c9840f90a56508d3d8c40
tduck973564 <tduck973564@gmail.com>
2024-04-30 01:37:14 +1000

    disable tlp

--- battery.sh
+++ battery.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Optimizes battery
-sudo dnf remove power-profiles-daemon
+#sudo dnf remove power-profiles-daemon
 
-sudo systemctl mask systemd-rfkill.service
-sudo systemctl mask systemd-rfkill.socket
-sudo systemctl enable --now NetworkManager-dispatcher
+#sudo systemctl mask systemd-rfkill.service
+#sudo systemctl mask systemd-rfkill.socket
+#sudo systemctl enable --now NetworkManager-dispatcher
 
-sudo dnf install tlp tlp-rdw powertop
+#sudo dnf install tlp tlp-rdw powertop
+sudo dnf install -y powertop
 
 sudo sh -c "echo 'PCIE_ASPM_ON_BAT=powersupersave
 PLATFORM_PROFILE_ON_BAT=low-power
@@ -20,8 +21,8 @@ CPU_SCALING_GOVERNOR_ON_BAT=schedutil
 CPU_BOOST_ON_BAT=0
 ' >> /etc/tlp.conf"
 
-sudo systemctl enable --now tlp
-sudo tlp-rdw enable
+#sudo systemctl enable --now tlp
+#sudo tlp-rdw enable
 
 sudo powertop --auto-tune
 sudo systemctl enable --now powertop
