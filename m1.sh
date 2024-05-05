65aff67fcb8554f4dab06ea6dcfc7788d76b1249
Thomas Duckworth <tduck973564@gmail.com>
2024-05-06 02:38:23 +1000

    memory optimisations for asahi

--- m1.sh
+++ m1.sh
@@ -149,3 +149,15 @@ sudo chattr +C /var/swap/swapfile
 sudo mkswap /var/swap/swapfile
 sudo chmod 0600 /var/swap/swapfile
 sudo swapon /var/swap/swapfile
+
+echo "Don't overcommit"
+sudo sysctl vm.overcommit_memory=2
+sudo sysctl vm.overcommit_ratio=30
+
+echo "Change zram algorithm"
+sudo touch /etc/modules-load.d/zstd.conf
+sudo sh -c "echo 'zstd' > /etc/modules-load.d/zstd.conf"
+sudo mkdir /etc/systemd/zram-generator.conf.d
+sudo touch /etc/systemd/zram-generator.conf.d/zram.conf
+sudo sh -c "echo '[zram0]
+compression-algorithm = zstd' > /etc/systemd/zram-generator.conf.d/zram.conf"
